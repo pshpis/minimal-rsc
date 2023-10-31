afc961137dd29f1bcb5e8cfecb347c03b1d576a9
phpis <p.shpis@inbox.ru>
2023-11-01 02:37:00 +0600

    feat: end copy&pate

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import MegaPage from './MegaPage';
 
 export default function App({page}) {
   return (
-    <div className="main">
-      <Nav />
-      <section key={page} className="col note-viewer">
-        <Suspense fallback={<p>Loading...</p>}>
-          <MegaPage page={page} />
-        </Suspense>
-      </section>
-    </div>
+    <>
+      <div className="main">
+        <Nav />
+        <section key={page} className="col note-viewer">
+          <Suspense fallback={<p>Loading...</p>}>
+            <MegaPage page={page} />
+          </Suspense>
+        </section>
+      </div>
+    </>
   );
 }
