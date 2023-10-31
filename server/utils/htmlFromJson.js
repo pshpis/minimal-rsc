d1fe6213b3333204c88b31c15cef67d3653d8b19
phpis <p.shpis@inbox.ru>
2023-10-31 14:55:42 +0600

    feat: end copy&pate

--- server/utils/htmlFromJson.js
+++ server/utils/htmlFromJson.js
@@ -1,13 +1,3 @@
-/**
- * @license React
- * react-server-dom-webpack-client.edge.development.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 'use strict';
 
 const React = require('react');
